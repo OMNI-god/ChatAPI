461d70aaa053ba73bdc39a718ea2d03964ed633f
Rupayan Mondal <jawda09@gmail.com>
2025-10-17 01:00:47 +0530

    chore: Update AssemblyInformationalVersion and cache file for consistency

--- ChatAPI/obj/Debug/net8.0/ChatAPI.AssemblyInfo.cs
+++ ChatAPI/obj/Debug/net8.0/ChatAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e0ec16b24e360bd593c90161303c2ae6ebdf2ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99ba1194a4f40ca4f8713782376ae16a3086ba42")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
